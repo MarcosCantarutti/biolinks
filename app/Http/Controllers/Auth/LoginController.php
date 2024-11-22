63871455e9bc09db282251533c19b3398c2b9a04
Marcos Cantarutti <marcossandim21@gmail.com>
2024-11-22 19:49:02 -0300

    Registrando um usuario inicio

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MakeLoginRequest;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(MakeLoginRequest $request)
+    {
+
+        if ($request->attempt()) {
+            return to_route('dashboard');
+        }
+
+        return back()->with(['message' => 'não encontrado']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'login']);
+
+
+Route::get('/dashboard', fn() => 'dashboard :: ' . auth()->id())
+    ->middleware('auth')->name('dashboard');
